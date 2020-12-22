372ae91c0791f7d7296b436cb5601d193bfc050f
manquintana <manuelalejandroquintana@gmail.com>
2020-12-22 15:57:03 -0300

    Final changes for the application. It lasts: Image on header, and reset button

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Text, TextInput, Alert, TouchableOpacity, Image, Linking} from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
+
+
+
 export default class Home extends Component {
   constructor(props) {
     super(props);    
@@ -19,56 +22,133 @@ export default class Home extends Component {
     };
   }
 
-  updateSuma() {
+  updateSuma = () => {
     this.setState({ suma: this.state.punto1 + this.state.punto2 * 2 + this.state.punto3 * 3 +
                       this.state.punto4 * 4 + this.state.punto5 * 5 + this.state.punto6 * 6 +
                       this.state.punto7 * 7 + this.state.punto8 * 8 + this.state.punto9 * 9 + this.state.punto10 * 10
     })
-    Alert.alert('updated')
+    console.log('Suma:' + this.state.suma )
+  }
+ 
+ 
+  resetAllOk = () => {
+    console.log('All is being deleted')
+    this.setState({
+      punto1: 0,
+      punto2: 0,
+      punto3: 0,
+      punto4: 0,
+      punto5: 0,
+      punto6: 0,
+      punto7: 0,
+      punto8: 0,
+      punto9: 0,
+      punto10: 0,
+      suma: 0 })
+    console.log('Suma:' + this.state.suma )
+  }
+  resetAll = () => {
+    Alert.alert(
+      'Reset values?',
+      'All progress will be deleted',      
+      [
+        {text: 'Yes, i want to reset', onPress: () => this.resetAllOk()},
+        {text: 'Cancel'},
+      ],
+      { cancelable: false }
+    )
   }
   updatePoint1 = (point1) => {
-    this.setState({ punto1: parseInt(point1) })
-    this.updateSuma()
+    console.log('Updated:'+ point1)
+    if(isNaN(point1)){
+      this.setState({ punto1: 0 })
+    }
+    else{
+      this.setState({ punto1: parseInt(point1) })
+    }
   }
   updatePoint2 = (point2) => {
-    this.setState({ punto2: parseInt(point2) })
-    this.updateSuma()
+    console.log('Updated:'+ point2)
+    if(isNaN(point2)){
+      this.setState({ punto2: 0 })
+    }
+    else{
+      this.setState({ punto2: parseInt(point2) })
+    }
   }
   updatePoint3 = (point3) => {
-    this.setState({ punto3: parseInt(point3) })
-    this.updateSuma()
+    console.log('Updated:'+ point3)
+    if(isNaN(point3)){
+      this.setState({ punto3: 0 })
+    }
+    else{
+      this.setState({ punto3: parseInt(point3) })
+    }
   }
   updatePoint4 = (point4) => {
-    this.setState({ punto4: parseInt(point4) })
-    this.updateSuma()
+    console.log('Updated:'+ point4)
+    if(isNaN(point4)){
+      this.setState({ punto4: 0 })
+    }
+    else{
+      this.setState({ punto4: parseInt(point4) })
+    }
   }
   updatePoint5 = (point5) => {
-    this.setState({ punto5: parseInt(point5) })
-    this.updateSuma()
+    console.log('Updated:'+ point5)
+    if(isNaN(point5)){
+      this.setState({ punto5: 0 })
+    }
+    else{
+      this.setState({ punto5: parseInt(point5) })
+    }
   }
   updatePoint6 = (point6) => {
-    this.setState({ punto6: parseInt(point6) })
-    this.updateSuma()
+    console.log('Updated:'+ point6)
+    if(isNaN(point6)){
+      this.setState({ punto6: 0 })
+    }
+    else{
+      this.setState({ punto6: parseInt(point6) })
+    }
   }
   updatePoint7 = (point7) => {
-    this.setState({ punto7: parseInt(point7) })
-    this.updateSuma()
+    console.log('Updated:'+ point7)
+    if(isNaN(point7)){
+      this.setState({ punto7: 0 })
+    }
+    else{
+      this.setState({ punto7: parseInt(point7) })
+    }
   }
   updatePoint8 = (point8) => {
-    this.setState({ punto8: parseInt(point8) })
-    this.updateSuma()
+    console.log('Updated:'+ point8)
+    if(isNaN(point8)){
+      this.setState({ punto8: 0 })
+    }
+    else{
+      this.setState({ punto8: parseInt(point8) })
+    }
   }
   updatePoint9 = (point9) => {
-    this.setState({ punto9: parseInt(point9) })
-    this.updateSuma()
+    console.log('Updated:'+ point9)
+    if(isNaN(point9)){
+      this.setState({ punto9: 0 })
+    }
+    else{
+      this.setState({ punto9: parseInt(point9) })
+    }
   }
   updatePoint10 = (point10) => {
-    this.setState({ punto10: parseInt(point10) })
-    this.updateSuma()
+    console.log('Updated:'+ point10)
+    if(isNaN(point10)){
+      this.setState({ punto10: 0 })
+    }
+    else{
+      this.setState({ punto10: parseInt(point10) })
+    }
   }
-
-  
-  
+ 
 
   render() {
     return (
@@ -85,31 +165,32 @@ export default class Home extends Component {
                 style= {styles.textinput}
                 keyboardType = 'number-pad'
                 onChangeText={numero => this.updatePoint10(parseInt(numero))}
-                //defaultValue={this.state.punto10.toString()}
-                value = {this.state.punto10.toString()}
+                //gotFocus = {}
+                //value = {this.state.punto10.toString()}
             >
               
             </TextInput>
             <TextInput
                 style= {styles.textinput}
                 keyboardType = 'number-pad'
-                defaultValue={this.state.punto9.toString()}
+                onChangeText={numero => this.updatePoint9(parseInt(numero))}
+                
             />
             <TextInput
                 style= {styles.textinput}
                 keyboardType = 'number-pad'
-                defaultValue={this.state.punto8.toString()}
+                onChangeText={numero => this.updatePoint8(parseInt(numero))}
                  
             />
             <TextInput
                 style= {styles.textinput}
                 keyboardType = 'number-pad'
-                defaultValue={this.state.punto7.toString()}
+                onChangeText={numero => this.updatePoint7(parseInt(numero))}
             />
             <TextInput
                 style= {styles.textinput}
                 keyboardType = 'number-pad'
-                defaultValue={this.state.punto6.toString()}
+                onChangeText={numero => this.updatePoint6(parseInt(numero))}
             />
             
         </View>
@@ -126,41 +207,42 @@ export default class Home extends Component {
         <TextInput
                 style= {styles.textinput}
                 keyboardType = 'number-pad'
-                defaultValue={this.state.punto5.toString()}
+                onChangeText={numero => this.updatePoint5(parseInt(numero))}
             />
             <TextInput
                 style= {styles.textinput}
                 keyboardType = 'number-pad'
-                defaultValue={this.state.punto4.toString()}
+                onChangeText={numero => this.updatePoint4(parseInt(numero))}
             />
             <TextInput
                 style= {styles.textinput}
                 keyboardType = 'number-pad'
-                defaultValue={this.state.punto3.toString()}
+                onChangeText={numero => this.updatePoint3(parseInt(numero))}
             />
             <TextInput
                 style= {styles.textinput}
                 keyboardType = 'number-pad'
-                defaultValue={this.state.punto2.toString()}
+                onChangeText={numero => this.updatePoint2(parseInt(numero))}
             />
             <TextInput
                 style= {styles.textinput}
                 keyboardType = 'number-pad'
-                defaultValue={this.state.punto1.toString()}
+                onChangeText={numero => this.updatePoint1(parseInt(numero))}
             />
         </View>
         <View style= {styles.buttonsView}>
           <View style={styles.button1}>
             <TouchableOpacity
                 style = {styles.buttonImage}
-                onPress={visitarWeb}>
+                onPress={() => this.updateSuma()}>
                 <Ionicons name="md-calculator" size={40} color="white"/> 
             </TouchableOpacity>
           </View>
           <View style={styles.button2}>
             <TouchableOpacity
                   style = {styles.buttonImage}
-                  onPress={visitarWeb}>
+                  onPress={() => this.resetAll()}>
+                  
                   <Ionicons name="md-refresh" size={40} color="white"/> 
             </TouchableOpacity>
           </View>
@@ -287,8 +369,9 @@ const styles = StyleSheet.create({
   linkWeb:{
     width: '100%',
     height:'auto',
-    position: 'absolute',
+    //position: 'absolute',
     bottom:0,
+    marginTop:'10%',
     alignItems:'center',
   }
 
